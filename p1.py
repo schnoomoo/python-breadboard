71a8d16efe590374fd081fcf737e76f466fdbebf
schnoomoo <121268076+schnoomoo@users.noreply.github.com>
2023-04-19 15:31:57 +1000

    Create p1.py

---
+++ p1.py
@@ -0,0 +1,5 @@
+for x in range(6,1,-1):
+   print (x)
+   
+   for x in range(5,0,-1):
+  print (x, 'little monkeys jumping on the bed, 1 fell off and bumped his head, momma called the doctor and the doctor said, no more monkeys jumping on the bed')
